acac546c7cee52885227c4e15b97e98d3effd042
yusuftengriverdi <45808617+yusuftengriverdi@users.noreply.github.com>
2020-05-03 01:16:38 +0300

    Add files via upload

--- Service_Announcer.py
+++ Service_Announcer.py
@@ -1,58 +1,74 @@
-import socket
-import time
-from multiprocessing import Process
 import glob
-import os
 import json
+import socket
+import threading
+import time
+from datetime import timedelta
 
 
-def run_in_parallel(*fns):
-    proc = []
-    for fn in fns:
-        p = Process(target=fn)
-        p.start()
-        proc.append(p)
-    for p in proc:
-        p.join()
+def send_broadcast(FILE_LIST, LOCAL_DICT, RECEIVER):
+    print("Last check was at ",
+          time.ctime())
+    print(LOCAL_DICT)
+    FILE_LIST = json.dumps(FILE_LIST)  # convert JSON array.
+    print("Current files: ", FILE_LIST)
+    LOCAL_DICT = json.dumps(LOCAL_DICT)
+    RECEIVER.send(bytes(LOCAL_DICT, "utf-8"))
+    RECEIVER.send(bytes(FILE_LIST, "utf-8"))
 
 
-def register_user(s, local_dict):
-    try:
-        client_info = s.recv(1024)
-        str_client_info = client_info.decode("utf-8")
-        s.send(bytes("Enter username! ", "utf-8"))
-        client_name = s.recv(1024)
-        str_client_name = client_name.decode("utf-8")
-        client_time = time.asctime()
-        client_file = s.recv(1024)
-        str_client_file = client_file.decode("utf-8")
-        local_dict.append((str_client_info, str_client_name, client_time, client_file))
-        print(local_dict)
-    except Exception as e:
-        print(str(e))
-        pass
+class Job(threading.Thread):
+    def __init__(self, interval, execute, *args, **kwargs):
+        threading.Thread.__init__(self)
+        self.daemon = False
+        self.stopped = threading.Event()
+        self.interval = interval
+        self.execute = execute
+        self.args = args
+        self.kwargs = kwargs
+
+    def stop(self):
+        self.stopped.set()
+        self.join()
+
+    def run(self):
+        while not self.stopped.wait(self.interval.total_seconds()):
+            self.execute(*self.args, **self.kwargs)
 
 
-def count_time(local_dict, tic_, toc_):
-    print(local_dict)
-
-    while True:
-        clock = tic_ - toc_
-        if int(clock) % 60 == 0:
-            print(local_dict)
-            print(" last update was at the time: " + time.asctime())
+def register_user(s, local_dict, message_dict):
+    client_info = s.recv(1024)
+    str_client_info = client_info.decode("utf-8")
+    s.send(bytes("Enter username! ", "utf-8"))
+    client_name = s.recv(1024)
+    str_client_name = client_name.decode("utf-8")
+    client_time = time.asctime()
+    client_file = s.recv(1024)
+    if not (str_client_name in message_dict.get("username")):
+        message_dict.get("username").append(str_client_name)
+    str_client_file = client_file.decode("utf-8")
+    local_dict.append((str_client_info, str_client_name, client_time, client_file))
+    message_dict.get("files").append(str_client_file)
+    print("Current users: ", local_dict)
+    print("Hosted files: ", message_dict)
 
 
 def main():
     # print(glob.glob("*"))
     filenames = glob.glob("*")  # read all files in the directory
     filenames_json = json.dumps(filenames)  # convert JSON array.
-    tic = time.time()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = socket.gethostname()
     port = 8080
     s.connect((host, port))
     print("Connected ... ")
+    local_dict = []
+    message_dict = {"username": [],
+                    "files": []}
+    WAIT_TIME_SECONDS = 60
+    job = Job(interval=timedelta(seconds=WAIT_TIME_SECONDS), execute=send_broadcast, FILE_LIST=filenames,
+              LOCAL_DICT=message_dict, RECEIVER=s)
+    job.start()
 
     msg = s.recv(1024)
     print("Host: ", msg.decode("utf-8"))
@@ -60,9 +76,6 @@ def main():
     s.send(bytes("Code0001", "utf-8"))
 
     ready = False
-    alert = s.recv(1024)
-    str_alert = alert.decode("utf-8")
-    print("Host: ", str_alert)
     code = s.recv(8)
     str_code = code.decode("utf-8")
     print("Host: ", str_code)
@@ -70,10 +83,9 @@ def main():
         s.send(bytes(filenames_json, "utf-8"))
         ready = True
     print(ready)
-    local_dict = []
-    register_user(s, local_dict)
-    while True:
-        run_in_parallel(register_user(s, local_dict), count_time(local_dict, tic, time.time()))
+    while ready:
+        filenames = glob.glob("*")  # read all files in the directory if anything is added
+        register_user(s, local_dict, message_dict)
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,34 +1,68 @@
 import socket
+import threading
 import time
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
+from datetime import timedelta
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # defining socket object
+host = socket.gethostname() # get this pc's host name
 port = 8080
 s.bind((host, port))
-s.listen(2)
+s.listen(2) # maximum listener number (increase that maybe)
 print(host)
 print("Waiting for any incoming connections ... ")
-conn, addr = '', ''
+ann, addr = '', ''
+
+def receive_broadcast(SENDER): # receive periodic messages
+    MESSAGE = SENDER.recv(2048)
+    MESSAGE_toSTR = MESSAGE.decode("utf-8")
+    print("Dictionary: ", MESSAGE_toSTR)
+    FILES = SENDER.recv(2048)
+    FILES_toSTR = FILES.decode("utf-8")
+    print("Dictionary: ", FILES_toSTR)
+
+class Job(threading.Thread): # provides parallel functioning method
+    def __init__(self, interval, execute, *args, **kwargs):
+        threading.Thread.__init__(self)
+        self.daemon = False
+        self.stopped = threading.Event()
+        self.interval = interval
+        self.execute = execute
+        self.args = args
+        self.kwargs = kwargs
+
+    def stop(self):
+        self.stopped.set()
+        self.join()
+
+    def run(self):
+        while not self.stopped.wait(self.interval.total_seconds()):
+            self.execute(*self.args, **self.kwargs)
+
+
 launch = False
-while not launch:
-    conn, addr = s.accept()
+while not launch:  # firstly connect the announcer
+    ann, addr = s.accept()
     print(addr, "Has connected to the server")
-    conn.send(bytes("Welcome to the server", "utf-8"))
-    ans = conn.recv(1024)
+    ann.send(bytes("Welcome to the server", "utf-8"))
+    ans = ann.recv(1024)
     str_ans = ans.decode("utf-8")
     print(addr, ": ", str_ans)
-    code = conn.recv(8)
+    code = ann.recv(8)
     str_code = code.decode("utf-8")
     if str_code == "Code0001":
-        conn.send(bytes("Be ready!", "utf-8"))
-        conn.send(bytes("Code0000", "utf-8"))
-        files = conn.recv(1024)
+        ann.send(bytes("Code0000", "utf-8"))
+        files = ann.recv(1024)
         launch = True
         print(launch)
-
 
 print(addr, " is Service_Announcer")
 ready = True
 # print(ready)
+WAIT_TIME_SECONDS = 60
+job = Job(interval=timedelta(seconds=WAIT_TIME_SECONDS), execute=receive_broadcast, SENDER=ann)
+job.start()
 
 while ready:
     try:
@@ -39,15 +73,15 @@ while ready:
         ans = client.recv(1024)
         str_ans = ans.decode("utf-8")
         print(address, ": ", str_ans)
-        conn.send(bytes(f"{address}", "utf-8"))
-        alert = conn.recv(1024)
+        ann.send(bytes(f"{address}", "utf-8"))
+        alert = ann.recv(1024)
         str_alert = alert.decode("utf-8")
         print("Announcer: ", str_alert)
         client.send(bytes(f"Announcer: {str_alert}", "utf-8"))
         user_name = client.recv(1024)
         str_user_name = user_name.decode("utf-8")
         print(address, ": My user name is ", str_user_name)
-        conn.send(bytes(f"{str_user_name}", "utf-8"))
+        ann.send(bytes(f"{str_user_name}", "utf-8"))
         ready_toFT = True
         print(ready_toFT)
         if ready_toFT:
@@ -56,7 +90,8 @@ while ready:
             file_data = file.read(1024)
             client.send(bytes(file_data))
             print(f"File {filename} is sent to user {str_user_name} at {time.asctime()}.")
-            conn.send(bytes(f"{filename}", "utf-8"))
+            ann.send(bytes(f"{filename}", "utf-8"))
     except Exception as e:
         print(str(e))
         pass
+
