b2ad21d467572f2013c2a8ba522db97077bb35e6
yusuftengriverdi <45808617+yusuftengriverdi@users.noreply.github.com>
2020-05-10 21:20:21 +0300

    Add files via upload

--- P2P_downloader.py
+++ P2P_downloader.py
@@ -74,7 +74,6 @@ def main():
             chunks = [filename + "_" + str(i) for i in range(1, 6)]
             with open("content.json", "r") as f:
                 content = json.load(f)
-            print(content)
             all_downloaded = True
             t = tqdm(total=5)
             t.set_description("Downloading (file %s)" % filename)
@@ -101,7 +100,7 @@ def main():
             if all_downloaded:
                 t.close()
                 # tqdm.write("\nAll chunks are successfully downloaded.")
-                combine_chunks(filename, "files", "files")
+                combine_chunks(filename, "temp", "downloads")
         except Exception as e:
             print("\n", e)
             pass
